7e98a6b1e052a6f24eaf051a73b8d54f0aedbab7
jalho <alho.jere@gmail.com>
2024-03-14 23:53:06 +0200

    Document

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
+-- [[
+-- 	Explore filesystem by `<Space>e`.
+-- ]]
 _G.explore = function()
     vim.cmd('Explore')
 end
-
 vim.api.nvim_set_keymap(
 	'n', '<Space>e', '<cmd>lua explore()<CR>',
 	{ noremap = true, silent = true })
 
+
+-- [[
+--	Use system clipboard.
+-- ]] 
 vim.o.clipboard = "unnamedplus"
 
