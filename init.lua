e1be11c0e9b187e32851d4ae28cd76c81280a33d
jalho <alho.jere@gmail.com>
2024-07-10 23:28:04 +0300

    Enable language server for Zig

--- init.lua
+++ init.lua
@@ -14,3 +14,25 @@ vim.api.nvim_set_keymap(
 -- ]] 
 vim.o.clipboard = "unnamedplus"
 
+
+-- [[
+--	Start a language server for Zig (zls) when editing 'zig' files.
+--
+--	Accessed 2024-07-10: https://github.com/zigtools/zls
+--
+--	To verify all's good, do ':checkhealth lsp' while having a 'zig' file
+--	open. It should show the 'zls' being active.
+--
+--	As of 2024-07-10, one of the features enabled by default by Neovim's
+--	LSP client is triggering code completion in INSERT mode by <C-X><C-O>.
+-- ]]
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'zig',
+  callback = function(args)
+    vim.lsp.start({
+      name = 'zls',
+   -- cmd = {'name-of-language-server-executable', '--option', 'arg1', 'arg2'},
+      cmd = {'zls'},
+    })
+  end,
+})
